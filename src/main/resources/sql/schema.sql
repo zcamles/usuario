fffae2f2747cc73ea7195655eecee472a5c5a229
zcamles <z.camacho.canales@gmail.com>
2023-01-14 23:17:44 -0300

    WIP: schema usuarios creado y inicializado junto con el proyecto

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA usuarios;
+
+SET SCHEMA usuarios;
+
+CREATE TABLE users ( 
+   id UUID PRIMARY KEY, 
+   name VARCHAR(50), 
+   email VARCHAR(20), 
+   password DATE,
+   created DATE,
+   modified DATE,
+   last_login DATE,
+   token VARCHAR(50),
+   is_active BOOL
+);
+
+CREATE TABLE phone (
+   number VARCHAR(10),
+   city_code VARCHAR(2),
+   country_code VARCHAR(2),
+   user_id UUID NOT NULL,
+   foreign key (user_id) references users(id)
+);
